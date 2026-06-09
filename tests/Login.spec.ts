f96e8ca6f55968dc3110a6821eeb9c95c9ee6407
Tanya <tayushka.pl@gmail.com>
2026-06-09 17:26:47 +0200

    refactor: move page setup to beforeEach

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -6,24 +6,26 @@ import { LoginPage } from "../pages/LoginPage";
 
 test.describe("Login Page", () => {
 
-    test("Log In with correct credentials", async ( {page} ) => {
+    let loginPage : LoginPage;
 
-        const loginPage = new LoginPage(page);
+    test.beforeEach(async ( {page} ) => {
+
+        loginPage = new LoginPage(page);
         await loginPage.visit();
+
+    })
+
+    test("Log In with correct credentials", async () => {
 
         const dashboardPage = await loginPage.logIn(users.standardUser.username, users.standardUser.password);
         await expect(dashboardPage.logoLocator).toBeVisible();
           
     });
 
-    test("Log in as a locked out user", async ({page}) => {
+    test("Log in as a locked out user", async () => {
 
-        const loginPage = new LoginPage(page);
-        await loginPage.visit();
         await loginPage.logInFailed(users.lockedOutUser.username, users.lockedOutUser.password);
         await expect(loginPage.errorMessage).toHaveText(errorMessageText.lockedOutUser.message);
         
-
-
     })
 });
